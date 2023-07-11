02360a9f72011e4379bb4c90faba06067c58a2cb
Artyom <raevskiy.7253@gmail.com>
2023-07-11 23:37:46 +0300

    done login and registration overlays

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ window.addEventListener("DOMContentLoaded", function() {
         menu.classList.remove("hidden");
     });
 
-
     window.addEventListener("click", function(event) {
         if(event.target.closest("#nav-menu-open")) {
             return;
@@ -21,8 +20,7 @@ window.addEventListener("DOMContentLoaded", function() {
             document.querySelector(".login_modal").classList.remove("hidden");
             requestAnimationFrame(function(){
                 document.querySelector(".login_modal").classList.add("modal_overlay_active");
-            });
-            
+            }); 
         });
     });
 
@@ -32,5 +30,22 @@ window.addEventListener("DOMContentLoaded", function() {
         setTimeout(function(){
             document.querySelector(".login_modal").classList.add("hidden");
         }, 200)
+    });
+
+    document.querySelector("#registration_button").addEventListener("click", function() {
+            document.querySelector(".login_modal").classList.remove("modal_overlay_active");
+            document.querySelector('.login_modal').classList.add("hidden");
+            setTimeout(function(){
+                document.querySelector(".registration_modal").classList.remove("hidden");
+                document.querySelector(".registration_modal").classList.add("modal_overlay_active");
+            }, 200);
+        });
+
+    document.querySelector(".registration_close_btn").addEventListener("click", function() {
+        document.querySelector(".registration_modal").classList.remove("modal_overlay");
+
+        setTimeout(function() {
+            document.querySelector(".registration_modal").classList.add("hidden");
+        });
     });
 });
