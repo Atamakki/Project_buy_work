cd73e8a0a46ad7757bede53a08ff09d81140d3f8
Artyom <raevskiy.7253@gmail.com>
2023-07-24 11:53:41 +0300

    added mobile version for profile

--- js/main.js
+++ js/main.js
@@ -51,20 +51,4 @@ window.addEventListener("DOMContentLoaded", function() {
         }, 200);
     });
 
-
-    const MenuButton = document.querySelector("#menu-open");
-
-    MenuButton.addEventListener("click", function(){
-        const menuP = document.querySelector(".mobile-menu-profile");
-        menuP.classList.remove("hidden");
-    });
-
-    window.addEventListener("click", function(eventP) {
-        if(eventP.target.closest("#menu-open")) {
-            return;
-        };
-        const menuP = document.querySelector(".mobile-menu-profile");
-        menuP.classList.add("hidden");
-    });
-
 });
